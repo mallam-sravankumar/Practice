b89019fa1b6db64d76a35291dca0cbad88d5f710
DELL <DELL@DESKTOP-9AF47S5>
2024-07-16 17:57:21 +0530

    l

---
+++ Practicse/src/Prac/EvenNumbers.java
@@ -0,0 +1,13 @@
+package Prac;
+//print the only even numbers from 60 to 30 using forloop
+public class EvenNumbers {
+public static void main(String[] args) {
+	for(int i=60;i>=30;i--)
+	{
+		if(i%2==0)
+		{
+		System.out.println(i);
+	}
+}
+}
+}
---
+++ Practicse/src/Prac/oddNumbers.java
@@ -0,0 +1,12 @@
+package Prac;
+//print only odd numbers from 10 to 25
+public class oddNumbers {
+public static void main(String[] args) {
+	for(int i=10;i<=25;i++)
+	{
+		if(i%2!=0)
+			
+			System.out.println(i);
+	}
+}
+}
