6726b7bb1d7e2041ad0e6397a42f4184e73754f9
DELL <DELL@DESKTOP-9AF47S5>
2024-07-16 16:40:40 +0530

    Statement of while,dowhile,switch

---
+++ Practicse/src/Prac/Sta3.java
@@ -0,0 +1,30 @@
+package Prac;
+
+public class Sta3 {
+public static void main(String[] args) {
+	int daynum=4;
+	switch(daynum)
+	{
+		case 1:System.out.println("Monday");
+		break;
+		
+		case 2:System.out.println("	Tuesday");
+		break;
+		
+		case 3:System.out.println("wednesday");
+		break;
+		case 4:System.out.println("Thursday");
+		case 5:System.out.println("Friday");
+		case 6:System.out.println("saturday");
+		break;
+		
+		case 7:System.out.println("Sunday");
+		break;
+
+		default:System.out.println("invalid day");
+		break;
+	}
+	
+
+}
+}
---
+++ Practicse/src/Prac/WhileLoop.java
@@ -0,0 +1,12 @@
+package Prac;
+
+public class WhileLoop {
+public static void main(String[] args) {
+	int i=60;
+	while(i>=30)
+	{
+		System.out.println(i);
+		i--;
+	}
+}
+}
