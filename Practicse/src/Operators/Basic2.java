381c28475463f1e7d59b68a4a13852699899a90e
DELL <DELL@DESKTOP-9AF47S5>
2024-07-16 17:33:07 +0530

    p

---
+++ Practicse/src/Operators/Basic2.java
@@ -0,0 +1,22 @@
+package Operators;
+//To check whether 23 is divisble by 13 and 17
+public class Basic2 {
+public static void main(String[] args) {
+	int a=23;
+	if(a%13==0&&a!=0)
+	{
+		System.out.println(a +"is divisble by 13");
+	}
+	else if(a%17==0&&a!=0)
+	{
+		System.out.println(a+"is divisble by 17");
+	}
+	else
+	{
+		System.out.println(a+ " is not divisble by 13 qnd 17");
+	}
+	{
+		
+	}
+}
+}
