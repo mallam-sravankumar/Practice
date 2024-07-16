84b2c11cab75893d1c6aeb1cc855e12eaf1eabc0
DELL <DELL@DESKTOP-9AF47S5>
2024-07-16 17:32:48 +0530

    _

---
+++ Practicse/src/Operators/Basic.java
@@ -0,0 +1,19 @@
+package Operators;
+//To ceck whether -143 is positive,negative or zero
+public class Basic {
+public static void main(String[] args) {
+	int num=-143;
+	if(num>0)
+	{
+		System.out.println(num+" is a positive");
+	}
+	else if(num<0)
+	{
+		System.out.println(num +"is a negative");
+	}
+	else
+	{
+		System.out.println(num+" is azer");
+	}
+}
+}
