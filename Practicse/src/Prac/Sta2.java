d598259e256f02ba52622cd6eb733f2cfa20cdab
DELL <DELL@DESKTOP-9AF47S5>
2024-07-16 16:12:20 +0530

    sta

---
+++ Practicse/src/Prac/Sta2.java
@@ -0,0 +1,21 @@
+package Prac;
+
+public class Sta2 {
+public static void main(String[] args) {
+	int num=7;
+	if(num%2==0&&num!=0) {
+		System.out.println(num+"is a even ");
+	}
+	else if(num%2!=0&&num!=0)
+	{
+		System.out.println(num +"is a odd");
+	}
+	else
+	{
+		System.out.println(num +"is zero");
+	}
+	{
+		
+	}
+}
+}
