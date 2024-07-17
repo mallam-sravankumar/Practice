f7cea1522eac436ab6c58cd265de3bc7baebee00
DELL <DELL@DESKTOP-9AF47S5>
2024-07-17 16:51:50 +0530

    str

---
+++ Practicse/src/Strings/Str2.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Str2 {
+public static void main(String[] args) {
+	String s="Java is high level language";
+	int l=s.length();
+	for(int i=0;i<l;i++)
+	{
+		char ch=s.charAt(i);
+		System.out.println(ch);
+	}
+}
+}
