9b8614c9b7f9de66eb3c107bd4a22ac4bf922a3a
DELL <DELL@DESKTOP-9AF47S5>
2024-07-17 15:04:55 +0530

    arr

---
+++ Practicse/src/BasicArrayProgramming/Arr2.java
@@ -0,0 +1,18 @@
+package BasicArrayProgramming;
+
+public class Arr2 {
+public static void main(String[] args) {
+	int a[];
+	a=new int[6];
+	a[0]=50;
+			a[1]=50;
+			a[2]=60;
+			a[3]=70;
+			a[4]=80;
+			a[5]=90;
+			for(int i=0;i<a.length;i++)
+			{
+				System.out.println("Elements of index is :"+i+" "+a[i]);
+			}
+}
+}
