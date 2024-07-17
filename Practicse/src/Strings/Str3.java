d6539368b0ffb20ebb1f6c7c3a9d1f2fd2bd6875
DELL <DELL@DESKTOP-9AF47S5>
2024-07-17 17:28:04 +0530

    str

---
+++ Practicse/src/Strings/Str3.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Str3 {
+	public static void main(String[] args) {
+		String s1="java development";
+		String str[]=s1.split("");
+		for(int i=str.length-1;i>=0;i--)
+		{
+			System.out.print(str[i]+" ");
+		}}
+	}
