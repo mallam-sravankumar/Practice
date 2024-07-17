d5fef4c02ba653b1b4c85f1ddd9a81312021c064
DELL <DELL@DESKTOP-9AF47S5>
2024-07-17 13:42:10 +0530

    ar

---
+++ Practicse/src/BasicArrayProgramming/Arr.java
@@ -0,0 +1,10 @@
+package BasicArrayProgramming;
+
+public class Arr {
+public static void main(String[] args) {
+	float f[ ]= {66.6f,77.5f,11.1f,59.7f};
+	{
+	System.out.println(f[0]+" "+f[1]+" "+f[2]+" "+f[3]);
+}
+}
+}
---
+++ Practicse/src/BasicArrayProgramming/Arrays.java
@@ -0,0 +1,14 @@
+package BasicArrayProgramming;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int a[]=new int [4];
+		a[0]=13;
+		a[1]=27;
+		a[2]=30;
+		a[3]=48;
+		System.out.println(a[0]+" "+a[1]+" "+a[2]+" "+a[3]);
+	}
+
+}
