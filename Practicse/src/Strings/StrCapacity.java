edb196e1189b5df26ddc19b19a3db7e0e27d7cbe
DELL <DELL@DESKTOP-9AF47S5>
2024-07-18 11:52:28 +0530

    str

---
+++ Practicse/src/Strings/StrCapacity.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class StrCapacity {
+public static void main(String[] args) {
+	StringBuffer sb=new StringBuffer();
+
+	System.out.println(	sb.capacity());
+	StringBuffer str1=new StringBuffer("java is a high level");
+	System.out.println(str1.capacity());
+}
+}
