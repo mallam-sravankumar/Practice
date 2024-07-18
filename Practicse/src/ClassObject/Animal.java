826698fda9f8bfd76a1c1c210e1a827b9169ca32
DELL <DELL@DESKTOP-9AF47S5>
2024-07-18 17:11:15 +0530

    anb

---
+++ Practicse/src/ClassObject/Animal.java
@@ -0,0 +1,23 @@
+package ClassObject;
+
+public class Animal{
+String DogName;
+String Breed;
+String color;
+
+Animal(String name,String Breed,String color)
+{
+	this.DogName=DogName;
+	this.Breed=Breed;
+	this.color=color;	
+}
+public String toString()
+{
+	return DogName+" "+Breed+" "+color;
+}
+public static void main(String[] args) {
+	Animal a=new Animal("Chinu","labrator","White");
+	String s=a.toString();
+	System.out.println(s);
+}
+}
