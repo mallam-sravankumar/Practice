4613f588ae897a72f004ebf5abd072862452c38f
DELL <DELL@DESKTOP-9AF47S5>
2024-07-18 11:41:17 +0530

    sb

---
+++ Practicse/src/Strings/StrBuff.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StrBuff {
+public static void main(String[] args) {
+	StringBuffer sb=new StringBuffer("java");
+	sb.append(" is high level");
+	System.out.println(sb);
+}
+}
