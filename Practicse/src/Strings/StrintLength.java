0af338faeca261850a43554daf18e42d8e96c6ee
DELL <DELL@DESKTOP-9AF47S5>
2024-07-18 15:07:20 +0530

    sb

---
+++ Practicse/src/Strings/StrintLength.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StrintLength {
+	public static void main(String[] args) {
+	String s="java is case";
+	StringBuilder sb=new StringBuilder(s);
+int a=sb.length();
+System.out.println(a);
+}
+}
