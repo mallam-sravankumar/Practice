2f77039381d70d2862dd2736b009c992917b8862
DELL <DELL@DESKTOP-9AF47S5>
2024-07-18 11:56:57 +0530

    str

---
+++ Practicse/src/Strings/StrChar.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StrChar {
+public static void main(String[] args) {
+	StringBuffer sb=new StringBuffer("java development");
+	char ch=sb.charAt(5);
+			System.out.println(ch);
+}
+}
