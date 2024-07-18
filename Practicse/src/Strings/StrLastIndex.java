095a8ba95c38e9f9f34edd7f63f2ba3d48849736
DELL <DELL@DESKTOP-9AF47S5>
2024-07-18 15:04:02 +0530

    sb

---
+++ Practicse/src/Strings/StrLastIndex.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StrLastIndex {
+	public static void main(String[] args) {
+	String s="java is case ";
+	StringBuilder sb=new StringBuilder(s);
+int a=sb.lastIndexOf("se");
+	System.out.println("LastINdexOf :"+a);
+}
+}
