c7c07d9ed170a253745aa3a1fa1b466363fcb04c
DELL <DELL@DESKTOP-9AF47S5>
2024-07-23 10:27:33 +0530

    a

---
+++ Practicse/src/BasicArrayProgramming/Ar.java
@@ -0,0 +1,16 @@
+package BasicArrayProgramming;
+
+public class Ar {
+	
+		public static void main(String[] args) {
+			int a[]=new int[] {10,20,30,40,50,60};
+			int l=a.length;
+			System.out.println(l);
+			for(int i=0;i<l;i++)
+			{
+				System.out.println(a[i]);
+			}
+				
+		}
+		}
+
